1a5704f6dda01698eff944e27ee9c337691ee1aa
Nihad <nihadamirv98@gmail.com>
2024-04-09 12:24:12 +0000

    update file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
-	node = (binary_tree_t*)malloc(sizeof(binary_tree_t));
+	node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
 	if (node == NULL)
 		return (NULL);
--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef BINARY_TREES_
-#define BINARY_TREES_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -11,28 +11,19 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
-
-// Basic Binary Tree
 typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s heap_t;
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-
-// Binary Search Tree
-typedef struct binary_tree_s bst_t;
-
-// AVL Tree
-typedef struct binary_tree_s avl_t;
-
-// Max Binary Heap
-typedef struct binary_tree_s heap_t;
-
 
 
 #endif
