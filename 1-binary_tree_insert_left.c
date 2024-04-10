9f4b0646d474c23bcfd439ceafd8c817950e3bd6
Rafig <rafigmammadov@yahoo.com>
2024-04-10 19:09:15 +0400

    Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -29,7 +29,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 	}
 
-	if (!((*parent).left))
+	if ((*parent).left != NULL)
 	{
 		(*node).left = (*parent).left;
 		(*parent).left->parent = node;
