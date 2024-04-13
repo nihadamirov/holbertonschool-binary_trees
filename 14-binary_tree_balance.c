9fe2a79d1c0524e7da322e6d65bfb976b54d2596
Nihad <nihadamirov98@gmail.com>
2024-04-13 10:45:40 +0000

    Add  function that measures the balance factor of a binary tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -30,8 +30,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
 		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-
-		return ((l > r) ? 1 : r);
+		return ((l > r) ? l : r);
 	}
 	return (0);
 }
