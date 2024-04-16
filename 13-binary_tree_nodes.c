f5947e992bf8582ea22fcdb949e66458ff084875
Rafig <rafigmammadov@yahoo.com>
2024-04-17 03:21:46 +0400

    Nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -14,17 +14,17 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-
-	size_t left_nodes = binary_tree_nodes(tree->left);
-	size_t right_nodes = binary_tree_nodes(tree->right);
-
-	size_t current_node_count = (tree->left || tree->right) ? 1 : 0;
+	size_t left_nodes, right_nodes, current_node_count;
 
 	if (!tree)
 	{
 		return (0);
 	}
 
+	left_nodes = binary_tree_nodes(tree->left);
+	right_nodes = binary_tree_nodes(tree->right);
+
+	current_node_count = (tree->left || tree->right) ? 1 : 0;
+
 	return (left_nodes + right_nodes + current_node_count);
 }
-
