cc9cf966a8bdb39334d7bf4691ca2ecfee3f3c2e
Rafig <rafigmammadov@yahoo.com>
2024-04-17 03:09:12 +0400

    Size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,32 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - Function that measures the size of a binary tree
+ *
+ * @tree: The parameter that rerpesents the pointer to the tree
+ *
+ * Return: Returns size of the binary tree
+ *
+ *
+ */
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_size, right_size;
+
+	if (!tree)
+	{
+		return (0);
+	}
+
+	if (!tree->left && !tree->right)
+	{
+		return (1);
+	}
+
+	left_size = binary_tree_size(tree->left);
+
+	right_size = binary_tree_size(tree->right);
+
+	return (left_size + right_size + 1);
+}
