a84c1535be4fb2762498debbbbd8933666a7c8be
Rafig <rafigmammadov@yahoo.com>
2024-04-17 03:32:53 +0400

    Is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -16,7 +16,7 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 
-	if (!(tree->left) && !(tree->right) == NULL)
+	if (!(tree->left) && !(tree->right))
 		return (1);
 
 	if ((tree->left && tree->right) &&
